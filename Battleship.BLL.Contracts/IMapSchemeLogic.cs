6129358743fe176cea6466e843f74635eb0a01a9
Adasda <asdasdas>
2020-10-18 20:22:16 +0300

    forgotten IMapSchemeLogic

---
+++ Battleship.BLL.Contracts/IMapSchemeLogic.cs
@@ -0,0 +1,15 @@
+﻿using Battleship.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Battleship.BLL.Contracts
+{
+    public interface IMapSchemeLogic
+    {
+        IEnumerable<MapScheme> GetMapSchemes();
+
+        int[,] GetMapSchemes(Guid id);
+
+        MapSchemeResult SaveMapSchemes(int[,] newMap);
+    }
+}
